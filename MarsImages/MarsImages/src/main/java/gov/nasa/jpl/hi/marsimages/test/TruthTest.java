0eaee060328ba529f13fc57803aea7717df13ca0
Mark Powell <drmarkpowell@gmail.com>
2014-09-26 22:47:44 -0700

    unit testing for Mars time

--- MarsImages/MarsImages/src/main/java/gov/nasa/jpl/hi/marsimages/test/TruthTest.java
+++ MarsImages/MarsImages/src/main/java/gov/nasa/jpl/hi/marsimages/test/TruthTest.java
@@ -1,7 +1,14 @@
 package gov.nasa.jpl.hi.marsimages.test;
 
+import android.test.InstrumentationTestCase;
+
 /**
  * Created by mpowell on 9/26/14.
  */
-public class TruthTest {
+public class TruthTest extends InstrumentationTestCase {
+    public void test() throws Exception {
+        final int expected = 5;
+        final int reality = 5;
+        assertEquals(expected, reality);
+    }
 }
