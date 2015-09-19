9df4aecf8af2808b6f14fdb754265f1033f5b0eb
Mark Powell <Mark.W.Powell@jpl.nasa.gov>
2015-09-19 14:53:25 -0700

    added fullscreen mode for image view with action bar icon to activate and state to persist across activity instances

--- MarsImages/MarsImages/src/main/java/gov/nasa/jpl/hi/marsimages/ui/ImageListActivity.java
+++ MarsImages/MarsImages/src/main/java/gov/nasa/jpl/hi/marsimages/ui/ImageListActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -33,6 +34,7 @@ import static gov.nasa.jpl.hi.marsimages.MarsImagesApp.MISSION_NAME_PREFERENCE;
 public class ImageListActivity extends AppCompatActivity implements ImageListFragment.Callbacks,
         ActionBar.OnNavigationListener {
 
+    public static final String STATE_HIDDEN_LIST = "hidden_list";
     public static final int IMAGE_LIST_REQUEST_CODE = 105;
     private SearchView searchView;
     private MenuItem mSearchItem;
@@ -44,18 +46,29 @@ public class ImageListActivity extends AppCompatActivity implements ImageListFra
     private boolean mTwoPane;
     private final int mRequestCode = IMAGE_LIST_REQUEST_CODE;
     private int imageViewPageNumber = 0;
+
 //    MenuItem mMapMenuItem;
     MenuItem mMosaicMenuItem;
+    MenuItem mHideShowListMenuItem;
+    ImageListFragment mListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_list);
 
+        mListFragment = (ImageListFragment)
+                getSupportFragmentManager().findFragmentById(R.id.image_list);
         if (imageViewPageNumber > 0) { //returning here after dismissing an ImageViewActivity
-            ImageListFragment listFragment = (ImageListFragment)
-                    getSupportFragmentManager().findFragmentById(R.id.image_list);
-            listFragment.mStickyList.setSelection(imageViewPageNumber);
+            mListFragment.mStickyList.setSelection(imageViewPageNumber);
+        }
+        if (savedInstanceState != null) {
+            boolean hiddenList = savedInstanceState.getBoolean(STATE_HIDDEN_LIST);
+            if (hiddenList) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.hide(mListFragment);
+                ft.commit();
+            }
         }
 
         if (findViewById(R.id.fragment_image_view_pager) != null) {
@@ -95,6 +108,11 @@ public class ImageListActivity extends AppCompatActivity implements ImageListFra
             EVERNOTE.loadMoreImages(this, true);
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(STATE_HIDDEN_LIST, mListFragment.isHidden());
+    }
     /**
      * Callback method from {@link ImageListFragment.Callbacks}
      * indicating that the item with the given ID was selected.
@@ -131,8 +149,18 @@ public class ImageListActivity extends AppCompatActivity implements ImageListFra
         if (mTwoPane)
             getMenuInflater().inflate(R.menu.image_view, menu);
 
+
 //        mMapMenuItem = menu.findItem(R.id.map);
         mMosaicMenuItem = menu.findItem(R.id.mosaic);
+        mHideShowListMenuItem = menu.findItem(R.id.hideShowList);
+        if (mHideShowListMenuItem != null && !mTwoPane) {
+            mHideShowListMenuItem.setVisible(false);
+        }
+        else if (mListFragment.isHidden()) {
+            mHideShowListMenuItem.setTitle("Show List");
+            mHideShowListMenuItem.setIcon(R.drawable.ic_action_return_from_full_screen);
+        }
+
         if (!MARS_IMAGES.hasLocations()) {
 //            MarsImagesApp.disableMenuItem(mMapMenuItem);
             MarsImagesApp.disableMenuItem(mMosaicMenuItem);
@@ -206,6 +234,22 @@ public class ImageListActivity extends AppCompatActivity implements ImageListFra
 //            case R.id.map:
 //                createMapActivity();
 //                return true;
+            case R.id.hideShowList:
+                if (mListFragment != null) {
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_left);
+                    if (mListFragment.isHidden()) {
+                        ft.show(mListFragment);
+                        mHideShowListMenuItem.setTitle("Hide List");
+                        mHideShowListMenuItem.setIcon(R.drawable.ic_action_full_screen);
+                    } else {
+                        ft.hide(mListFragment);
+                        mHideShowListMenuItem.setTitle("Show List");
+                        mHideShowListMenuItem.setIcon(R.drawable.ic_action_return_from_full_screen);
+                    }
+                    ft.commit();
+                }
+                return true;
             case R.id.mosaic:
                 createMosaicActivity();
                 return true;
